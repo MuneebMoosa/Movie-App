f82c7ce4395e4735859ca3c85e49f1150c84c8af
MuneebMoosa <muneebmoosavv@gmail.com>
2025-10-24 11:43:47 +0530

    tailwind setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <h1 className="text-3xl font-bold underline">Hello World!!!</h1>
   )
-}
+};
 
 export default App
